45a37c2ea685116753263f787c1e6854c9d7b82c
hueyren03-jpg <hueyren03@gmail.com>
2026-09-18 16:58:39 +0800

    Add stock transfer save result

--- SenangRetails.Shared/Models/DTOs/StockTransferDTO.cs
+++ SenangRetails.Shared/Models/DTOs/StockTransferDTO.cs
@@ -4,6 +4,18 @@ using System.Linq;
 
 namespace SenangRetails.Shared.Models.DTOs
 {
+
+    public sealed class StockTransferSaveResult
+    {
+        public bool TransferSaved { get; init; }
+        public bool GinSaved { get; init; }
+        public bool Success => TransferSaved && GinSaved;
+        public bool IsPartialSuccess => TransferSaved && !GinSaved;
+        public string Message { get; init; } = "";
+        public string? TransferDocumentId { get; init; }
+        public string? GinDocumentId { get; init; }
+    }
+
     public class StockTransferModel
     {
         public string DocNo { get; set; } = "";
